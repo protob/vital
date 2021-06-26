5f59a9c58d1039b31f4fec4ff5d1aebc77e40704
protob <protobx@gmail.com>
2021-06-26 16:47:52 +0200

    add protium config

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,38 @@
-import { ViteSSG } from 'vite-ssg'
+import {  ViteSSG} from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
-import { setupLayouts } from 'virtual:generated-layouts'
+import { setupLayouts} from 'virtual:generated-layouts'
 import App from './App.vue'
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 import './styles/main.css'
+import { createApp} from "vue";
+import Protium from "@protium/protium";
+import "@protium/protium/dist/protium.css";
+import "@protium/protium/dist/protium-full-vars.css";
+import FontAwesomeIcon from "./config/icons"
+
+
+
 
 const routes = setupLayouts(generatedRoutes)
 
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
-  App,
-  { routes },
-  (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
-  },
+    App, {
+        routes,
+        //base: 'dist',
+    },
+    (ctx) => {
+        // install all modules under `modules/`
+        ctx.app.component("vue-fontawesome", FontAwesomeIcon)
+        ctx.app.use(
+            Protium,
+            {
+                iconComponent: "vue-fontawesome",
+                iconPack: "fas",
+            }
+        )
+        Object.values(
+            import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
+    },
 )
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,14 +12,30 @@ const go = () => {
 }
 
 const { t } = useI18n()
+
+const popupMsg=(msg)=>{
+alert(msg)
+}
+
 </script>
- 
+
 <template>
   <div>
-    <h1 class="text-4xl">
-   VITAL
-    </h1>
+    <h1 class="text-4xl text-center">VITAL</h1>
 
+    <div class="container mx-auto  pt-6">
+      <div class="flex flex-row  justify-center">
+        <div class="mr-2"> 
+        <prt-button @click="popupMsg('Hello')">Click me</prt-button>
+        </div>
+
+
+    <router-link  to="/about" :title="t('button.about')">
+        <prt-button>About</prt-button>
+    </router-link>
+
+      </div>
+    </div>
   </div>
 </template>
 
